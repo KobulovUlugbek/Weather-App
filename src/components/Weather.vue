abe7f44fa95616e9faa618daf2f5d35a16c6824b
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-10-30 06:36:31 +0100

    added search

---
+++ src/components/Weather.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="app">
+       <h1>Weather App will be hier</h1>
+    </div>
+</template>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+<script>
+
+export default {
+    name: 'myWeatherApp',
+  components: {
+  
+  },
+}
+
+
+</script>
+  
+  
+  
+  
+  
+  
+  
+<style></style>
+  
